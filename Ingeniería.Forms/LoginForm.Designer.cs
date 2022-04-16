e827f332499fb7ef93cd2957933c60df4019188f
Darker3424 <alessander2907@gmail.com>
2022-04-16 00:34:51 -0500

    Ingeso de forms 1 y 2

--- Ingeniería.Forms/LoginForm.Designer.cs
+++ Ingeniería.Forms/LoginForm.Designer.cs
@@ -29,11 +29,95 @@ partial class LoginForm
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "LoginForm";
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(178, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(38, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Correo :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(38, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña :";
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtCorreo.Location = new System.Drawing.Point(148, 98);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(265, 29);
+            this.txtCorreo.TabIndex = 3;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtContraseña.Location = new System.Drawing.Point(148, 137);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(265, 29);
+            this.txtContraseña.TabIndex = 4;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnIngresar.Location = new System.Drawing.Point(178, 194);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(117, 30);
+            this.btnIngresar.TabIndex = 5;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(478, 236);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
     }
 
     #endregion
+
+    private Label label1;
+    private Label label2;
+    private Label label3;
+    private TextBox txtCorreo;
+    private TextBox txtContraseña;
+    private Button btnIngresar;
 }
---
+++ Ingeniería.Forms/MainMenu.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Ingeniería.Forms
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnFrmRegistrarProducto = new System.Windows.Forms.Button();
+            this.btnFrmRegistrarPedido = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(156, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MENU PRINCIPAL";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnFrmRegistrarPedido);
+            this.groupBox1.Controls.Add(this.btnSalir);
+            this.groupBox1.Controls.Add(this.btnFrmRegistrarProducto);
+            this.groupBox1.Location = new System.Drawing.Point(12, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(571, 214);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnFrmRegistrarProducto
+            // 
+            this.btnFrmRegistrarProducto.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnFrmRegistrarProducto.Location = new System.Drawing.Point(48, 106);
+            this.btnFrmRegistrarProducto.Name = "btnFrmRegistrarProducto";
+            this.btnFrmRegistrarProducto.Size = new System.Drawing.Size(475, 42);
+            this.btnFrmRegistrarProducto.TabIndex = 0;
+            this.btnFrmRegistrarProducto.Text = "Formulario Registrar Productos";
+            this.btnFrmRegistrarProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnFrmRegistrarPedido
+            // 
+            this.btnFrmRegistrarPedido.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnFrmRegistrarPedido.Location = new System.Drawing.Point(48, 37);
+            this.btnFrmRegistrarPedido.Name = "btnFrmRegistrarPedido";
+            this.btnFrmRegistrarPedido.Size = new System.Drawing.Size(475, 42);
+            this.btnFrmRegistrarPedido.TabIndex = 0;
+            this.btnFrmRegistrarPedido.Text = "Formulario Registrar Pedidos";
+            this.btnFrmRegistrarPedido.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSalir.Location = new System.Drawing.Point(435, 172);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(130, 36);
+            this.btnSalir.TabIndex = 0;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(595, 298);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private Button btnFrmRegistrarPedido;
+        private Button btnSalir;
+        private Button btnFrmRegistrarProducto;
+    }
+}
