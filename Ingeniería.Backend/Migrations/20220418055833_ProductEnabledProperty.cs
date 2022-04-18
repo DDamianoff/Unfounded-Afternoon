8b9ecc2f591aef3063f6415912b79546ad6ca684
Dino Damianoff <75574873+TeiV2@users.noreply.github.com>
2022-04-18 06:25:49 -0300

    The king is dead: dataGridView is no more.

---
+++ Ingeniería.Backend/Migrations/20220418055833_ProductEnabledProperty.cs
@@ -0,0 +1,340 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ingeniería.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductEnabledProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedidos_Usuarios_VendedorId",
+                table: "Pedidos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Categorías_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Marcas_BrandId",
+                table: "Product");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Pedidos_OrderId",
+                table: "Product");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Product",
+                table: "Product");
+
+            migrationBuilder.RenameTable(
+                name: "Product",
+                newName: "Productos");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_OrderId",
+                table: "Productos",
+                newName: "IX_Productos_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_CategoryId",
+                table: "Productos",
+                newName: "IX_Productos_CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_BrandId",
+                table: "Productos",
+                newName: "IX_Productos_BrandId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VendedorId",
+                table: "Pedidos",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Pedidos",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Monedas",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Marcas",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RazónSocial",
+                table: "Comprobantes",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Dirección",
+                table: "Comprobantes",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Categoría",
+                table: "Categorías",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Productos",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripción",
+                table: "Productos",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "Productos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Productos",
+                table: "Productos",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedidos_Usuarios_VendedorId",
+                table: "Pedidos",
+                column: "VendedorId",
+                principalTable: "Usuarios",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Productos_Categorías_CategoryId",
+                table: "Productos",
+                column: "CategoryId",
+                principalTable: "Categorías",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Productos_Marcas_BrandId",
+                table: "Productos",
+                column: "BrandId",
+                principalTable: "Marcas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Productos_Pedidos_OrderId",
+                table: "Productos",
+                column: "OrderId",
+                principalTable: "Pedidos",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedidos_Usuarios_VendedorId",
+                table: "Pedidos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Productos_Categorías_CategoryId",
+                table: "Productos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Productos_Marcas_BrandId",
+                table: "Productos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Productos_Pedidos_OrderId",
+                table: "Productos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Productos",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "Productos");
+
+            migrationBuilder.RenameTable(
+                name: "Productos",
+                newName: "Product");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Productos_OrderId",
+                table: "Product",
+                newName: "IX_Product_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Productos_CategoryId",
+                table: "Product",
+                newName: "IX_Product_CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Productos_BrandId",
+                table: "Product",
+                newName: "IX_Product_BrandId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VendedorId",
+                table: "Pedidos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Pedidos",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Monedas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Marcas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RazónSocial",
+                table: "Comprobantes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Dirección",
+                table: "Comprobantes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Categoría",
+                table: "Categorías",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Product",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripción",
+                table: "Product",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Product",
+                table: "Product",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedidos_Usuarios_VendedorId",
+                table: "Pedidos",
+                column: "VendedorId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Categorías_CategoryId",
+                table: "Product",
+                column: "CategoryId",
+                principalTable: "Categorías",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Marcas_BrandId",
+                table: "Product",
+                column: "BrandId",
+                principalTable: "Marcas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Pedidos_OrderId",
+                table: "Product",
+                column: "OrderId",
+                principalTable: "Pedidos",
+                principalColumn: "Id");
+        }
+    }
+}
